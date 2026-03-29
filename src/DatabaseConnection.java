c15125d4525ccddea3489c74c67ae869c3e99cc1
osmit <n12424722@qut.edu.au>
2026-03-29 12:15:37 +1000

    added database and connected it to main

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    /** The singleton instance of the database connection. */
+    private static Connection instance = null;
+    /** Constructor intializes the connection. */
+    private DatabaseConnection() {
+        String url = "jdbc:sqlite:pets.db";
+        try {
+            instance = DriverManager.getConnection(url);
+        } catch (SQLException sqlEx) {
+            System.err.println(sqlEx);
+        }
+    }
+
+    /** Provides global access to the singleton instance of the database connection. * @return a handle to the singleton instance of the database connection.*/
+    public static Connection getInstance() {
+        if (instance == null) {
+            new DatabaseConnection();
+        }
+        return instance;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -10,6 +12,30 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+        }
+
+        // connects to the database
+        Connection connection = DatabaseConnection.getInstance();
+        try {
+            Statement statement = connection.createStatement();
+            // Users is a table in the database with an autoincrementing id, varchar username and a varchar password.
+            // example code
+            // statement.execute("CREATE TABLE IF NOT EXISTS Users (id INTEGER AUTO_INCREMENT PRIMARY KEY , username VARCHAR, password VARCHAR)");
+
+            // Code for inserting a new user with admin for username and password for password
+            // statement.execute("INSERT INTO Users(username, password) VALUES ('admin', 'password')");
+
+            // selects all users and saves it to x
+            ResultSet x = statement.executeQuery("SELECT * FROM Users");
+            // points to the first value of x using the cursor
+            x.next();
+            // prints the string of the username
+            System.out.println(x.getString("username"));
+            // closes the connection (do this everytime you use the database)
+            connection.close();
+            // error catching
+        } catch (SQLException sqle) {
+            System.err.println(sqle);
         }
     }
 }
